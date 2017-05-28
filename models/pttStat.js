928d052a099a9726bc12cec5d29745cfb0012b9d
r05921104 <r05921104@ntu.edu.tw>
2017-05-28 11:15:30 +0800

    'first'

---
+++ models/pttStat.js
@@ -0,0 +1,13 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var pttStatSchema = new Schema({
+	word:{
+		type:String
+	},
+	number:{
+		type:Number
+	},
+});
+
+module.exports=mongoose.model('PttStat',pttStatSchema);
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var pttStat = require('../models/pttStat');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/pttStat',function(req,res,next){
+	pttStat.find(function(err, stats){
+		res.json(stats[0]);
+	});
+});
+
+router.get('/postOne',function(res,res,next){
+	var newStat = new pttStat();
+	newStat.word = 'hello';
+	newStat.number = 12;
+	newStat.save(function(err,stat){
+	    if(err){
+	      console.log('無法存入');
+	      return;
+	    }
+	    console.log(stat);
+	    console.log('成功存入');
+	 });
+});
+
+module.exports = router;
